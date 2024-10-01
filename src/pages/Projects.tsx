01ab2ccb3ba1a80adf78ffe83daae2caafa8bdfb
Thomas VELU <velu.thomas77@laposte.net>
2024-10-01 18:03:00 +0200

    change : replaced fetch by local data

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,112 +1,84 @@
-import { useEffect, useState } from 'react';
-import { Container, Typography, Card, CardContent, Button } from '@mui/material';
-
-interface Project {
-    id: number;
-    name: string;
-    description: string | null;
-    html_url: string;
-    owner: { avatar_url: string };
-    stars?: number;
-    languages: string[];
-}
-
-const fetchGitHubProjects = async (username: string): Promise<Project[]> => {
-    try {
-        const response = await fetch(`https://api.github.com/users/${username}/repos`);
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const projects = await response.json();
-        const projectsWithLanguages = await Promise.all(projects.map(async (repo: any) => {
-            const langResponse = await fetch(repo.languages_url);
-            const languages = await langResponse.json();
-            const languageList = Object.keys(languages);
-    
-            return {
-                id: repo.id,
-                name: repo.name,
-                description: repo.description,
-                html_url: repo.html_url,
-                owner: repo.owner,
-                languages: languageList // Ajoute les langages ici
-            };
-        }));
-        return projectsWithLanguages;
-    } catch (error) {
-        console.error('Error fetching GitHub projects:', error);
-        return [];
-    }
-};
-
+import { Container, Typography, Card, CardContent, Button, Box, Link } from '@mui/material';
+import projectsData from '../assets/Projects.json';
+import { useTranslation } from 'react-i18next';
 
 const Projects = () => {
-    const [projects, setProjects] = useState<Project[]>([]);
-
-    useEffect(() => {
-        const fetchProjects = async () => {
-            const fetchedProjects = await fetchGitHubProjects("tvelu77");
-            setProjects(fetchedProjects);
-        };
-        fetchProjects();
-    }, []);
-
+    const { t } = useTranslation();
     return (
-        <Container>
-            <Typography variant="h4" component="h1" gutterBottom>
-                GitHub Projects
-            </Typography>
-            <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-                {projects.length > 0 ? (
-                    projects.map((project) => (
-                        <Card 
-                            key={project.id} 
-                            variant="outlined" 
-                            sx={{
-                                position: 'relative',
-                                overflow: 'hidden',
-                                height: '300px',
-                                width: '100%',
-                                backgroundColor: 'rgba(0, 0, 0, 0.1)', // Couleur de fond
-                                '&:hover': {
-                                    '& .overlay': {
-                                        opacity: 0.8,
+        <Box
+            sx={{
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: '20px',
+            }}
+        >
+            <Container
+                sx={{
+                    padding: '20px',
+                    borderRadius: '8px',
+                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                }}
+            >
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+                    {projectsData.length > 0 ? (
+                        projectsData.map((project) => (
+                            <Card
+                                key={project.id}
+                                variant="outlined"
+                                sx={{
+                                    position: 'relative',
+                                    overflow: 'hidden',
+                                    height: '300px',
+                                    width: '100%',
+                                    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+                                    '&:hover': {
+                                        '& .overlay': {
+                                            opacity: 0.8,
+                                        }
                                     }
-                                }
-                            }}
-                        >
-                            <CardContent sx={{ position: 'relative', zIndex: 1 }}>
-                                <Typography variant="h5" component="div" color="text.primary">
-                                    {project.name}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    {project.description || 'No description available.'}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
-                                    {project.languages.length > 0 
-                                        ? `Languages: ${project.languages.join(', ')}` 
-                                        : 'No languages available.'
-                                    }
-                                </Typography>
-                                <Button 
-                                    variant="contained" 
-                                    href={project.html_url} 
-                                    target="_blank" 
-                                    rel="noopener noreferrer"
-                                    sx={{ mt: 2 }}
-                                >
-                                    View on GitHub
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    ))
-                ) : (
-                    <Typography variant="body1" color="text.secondary">
-                        No projects available.
-                    </Typography>
-                )}
-            </div>
-        </Container>
+                                }}
+                            >
+                                <CardContent sx={{ position: 'relative', zIndex: 1 }}>
+                                    <Typography variant="h5" component="div" color="text.primary">
+                                        {project.name}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {project.description || t('projects.no-description')}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
+                                        {project.languages.length > 0
+                                            ? `${t('projects.languages')} ${project.languages.join(', ')}`
+                                            : `${t('projects.no-languages')}`
+                                        }
+                                    </Typography>
+                                    <Button
+                                        variant="contained"
+                                        href={project.url}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        sx={{ mt: 2 }}
+                                    >
+                                        {t('projects.to-github')}
+                                    </Button>
+                                </CardContent>
+                            </Card>
+                        ))
+                    ) : (
+                        <Typography variant="body1" color="text.secondary">
+                            {t('projects.no-projects')}
+                        </Typography>
+                    )}
+                    <Link href="https://github.com/tvelu77" underline="none" sx={{ mt: 2, textAlign: 'center', color: 'primary.main' }}>
+                        <Typography variant="body1" color="primary">
+                        {t('projects.much-more')}
+                        </Typography>
+                    </Link>
+                </div>
+            </Container>
+        </Box>
     );
 };
 
